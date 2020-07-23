b27fbdc7f16a213d5f17d291f145ae200fba1365
Francesco <francescofinotto@hotmail.com>
2020-07-23 12:03:46 +0200

    added orbit controls + texture loading

--- js/resources.js
+++ js/resources.js
@@ -2,5 +2,6 @@
  * Define Resources path
  */
 class Resources{
-
+    static HeightMap=()=>'textures/heightmap2.png';
+    static TextureCube=()=>'textures/11635.jpg';
 }
--- js/site.js
+++ js/site.js
@@ -1,6 +1,32 @@
 let canvas = new CanvasHandler("body");
+let controls = new THREE.OrbitControls( canvas.camera, canvas.renderer.domElement );
+let geo,mat,mesh;
+let heightMap;
+
+
+canvas.camera.position.z = 5;
+loadTerrain();
 
 canvas.update =()=>{
 
 }
 canvas.renderLoop();
+
+function loadResources(){
+    let text = new THREE.TextureLoader().load(Resources.TextureCube(),(text)=>{
+        geo = new THREE.BoxGeometry(1,1,1);
+        mat = new THREE.MeshBasicMaterial({map:text});
+        mesh = new THREE.Mesh(geo,mat);
+        canvas.scene.add(mesh);
+    });
+}
+
+function loadTerrain() {
+    let text = new THREE.TextureLoader().load(Resources.HeightMap(),(text)=>{
+        this.heightMap = text;
+        generateTerrain();
+    });
+}
+function generateTerrain(){
+
+}
