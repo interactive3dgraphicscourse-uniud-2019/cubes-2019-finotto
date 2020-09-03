c4ac95ee4976ed6aa9726713a0a52922f2f3aa9c
Francesco finotto <francescofinotto@hotmail.com>
2020-09-04 01:44:50 +0200

    removed old resources path

--- js/resources.js
+++ js/resources.js
@@ -4,14 +4,5 @@
 class Resources{
 
     static HeightMap = ()=>'textures/heightmap2.png';
-    static TextureBackground = ()=>'textures/cubemaps/'
-    static TextureGround = ()=>'textures/moss/moss-albedo.jpg';
-    static TextureNormalGround = ()=>'textures/moss/moss-normals.jpg';
-    static TextureRoughnessGround = ()=>'textures/moss/moss-roughness.jpg';
-    static TexturePaving = ()=>'textures/paving/Paving-albedo.jpg';
-    static TextureNormalPaving = ()=>'textures/paving/Paving-normals.jpg';
-    static TextureRoughnessPaving = ()=>'textures/paving/Paving-roughness.jpg';
-    static TextureIce = ()=>'textures/ice/ice-albedo.jpg';
-    static TextureNormalIce = ()=>'textures/ice/ice-normals.jpg';
-    static TextureRoughnessIce = ()=>'textures/ice/ice-roughness.jpg';
+
 }
